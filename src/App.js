64ac37b45e5276f1400ab8023775ab9bc11e90b2
rajan <rajangreedge@gmail.com>
2018-12-12 20:36:05 +0530

    fix props for MapContainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import locations from "./location.json"
+import locations from './location.json'
+import MapContainer from './components/MapDisplay'
 class App extends Component {
   state= {
     lat:28.627956,
@@ -15,7 +16,12 @@ class App extends Component {
         <header className="App-header">
           <h1 className="main-heading"> Metro Location Delhi </h1>
         </header>
-        
+        <MapContainer
+          lat={this.state.lat}
+          lng={this.state.lng}
+          zoom={this.state.zoom}
+          locations={this.state.allLoc}
+        />
       </div>
     );
   }
