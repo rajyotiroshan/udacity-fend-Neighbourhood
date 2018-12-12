df2f1516395eb70c288494efcbdcbee99274eccf
rajan <rajangreedge@gmail.com>
2018-12-12 20:09:21 +0530

    added state to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import locations from "./location.json"
 class App extends Component {
+  state= {
+    lat:28.627956,
+    lng:77.295627,
+    zoom:13,
+    allLoc:locations
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="main-heading">Your NHood </h1>
+          <h1 className="main-heading"> Metro Location Delhi </h1>
         </header>
+        
       </div>
     );
   }
